60046d2292dd2b0e9ab47a26cfa54a5a7a7d9af5
TimeFur <djs860403@gmail.com>
2018-01-18 00:24:31 +0800

    Create the module chat function & using the beautifulsoup for parsing

--- twitch_chat.py
+++ twitch_chat.py
@@ -1,20 +1,31 @@
 import requests
 import logging
 import json
+from bs4 import BeautifulSoup
 from selenium import webdriver
 
 driver = webdriver.PhantomJS()
 logging.basicConfig(level = logging.DEBUG)
 
-def main():
-    _url  = "https://www.twitch.tv/mobilmobil"
 
-    session = requests.Session()
+def twitch_chat(driver, _url):
+    twitch_pattern = "chat-line__message"
+
     driver.get(_url)
     
     while driver.page_source.find("chat-line__message") < 0:
-        pass    
-    print (driver.page_source)
+        pass
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    #print (soup.prettify())
+    l = (soup.find_all("div", class_="chat-line__message"))
+    for i in l:
+        print ("---------------")
+        print (i)
+
+def main():
+    _url  = "https://www.twitch.tv/kandytung"
+    twitch_chat(driver, _url)
     
 if __name__ == "__main__":
     main()
