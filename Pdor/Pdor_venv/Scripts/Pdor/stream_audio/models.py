43b573808add65a420a5dd623153f97927342afa
TimeFur <djs860403@gmail.com>
2018-05-27 23:56:21 +0800

    Models DB setting
    1. Setting the DB type in setting.py
    2. In the Model.py, add the Class to declare the database attribution
    
    3-1. [python manage.py makemigration] it would transfer the Model.py to
    the data scripts of 000X_initial.py
    3-2. [python manage.py migrate] it would update the model.py to the
    db_sqlite

--- Pdor/Pdor_venv/Scripts/Pdor/stream_audio/models.py
+++ Pdor/Pdor_venv/Scripts/Pdor/stream_audio/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Stream_DB(models.Model):
+    title = models.CharField(max_length = 50)
+    url = models.TextField(blank = True)
+    photo = models.URLField(blank = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    
