99944f1167b3a46b5840d99316ca4f80ba3c3eb0
TimeFur <djs860403@gmail.com>
2018-05-27 23:27:35 +0800

    Template setting
    1. Create the timeplate folder, set as the same layer as app
    2. Setting the path to the template folder (setting.py)
    3. Reponse the HttpRequest object with template & dict

--- Pdor/Pdor_venv/Scripts/Pdor/stream_audio/views.py
+++ Pdor/Pdor_venv/Scripts/Pdor/stream_audio/views.py
@@ -3,7 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
+MAIN_TEMPLATE = 'Main.html'
 
 # Create your views here.
 def get_stream_url(request):
-    return HttpResponse("Stream URL")
+
+    main_html_dict = {'current_time' : str(datetime.now())}
+    
+    #return HttpResponse("Stream URL")
+    return render(request,
+                  MAIN_TEMPLATE,
+                  main_html_dict)
