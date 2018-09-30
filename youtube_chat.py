c57adf00e340f3af6d3ed77965da7b32094212ca
TimeFur <djs860403@gmail.com>
2018-09-30 22:56:38 +0800

    Using the selenium to access the chat room

--- youtube_chat.py
+++ youtube_chat.py
@@ -7,6 +7,10 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+
+import logging as log
+
 '''
 CLIENT_SECRETS_FILE = 'client_secret.json'
 
@@ -35,8 +39,9 @@ def channels_list_by_username(service, **kwargs):
 
 def chat_show(url):
     url = url.replace('watch', 'live_chat')
-
-    r = requests.get(url)
+    print (url)
+    session = requests.Session()
+    r = session.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
     print r.text
@@ -44,10 +49,24 @@ def chat_show(url):
     msg = soup.find_all('yt-live-chat-text-message-renderer')
     for tag in msg:
         print tag.string
-        
-def main():
+
+def Chatroom_flow():
     url = raw_input("URL = ")
     chat_show(url)
+    
+def main():
+    
+    #https://www.youtube.com/watch?v=u5X_hiHtKkM
+    chrome_path = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+    driver = webdriver.Chrome(chrome_path)
+    web_request = driver.get("https://www.youtube.com/live_chat?v=u5X_hiHtKkM")
+
+    #soup = BeautifulSoup(driver.page_source, 'html.parser')
+    #msg = soup.find_all('yt-live-chat-text-message-renderer')
+    #print msg
+    #ps = driver.page_source
+    #print ps
+    
     
 if __name__ == "__main__":
     main()
